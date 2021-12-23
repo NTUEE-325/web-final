a865499604fbe241125db724b90f30377e8c86b9
hackhaha1 <hankchui88@gmail.com>
2021-12-23 15:42:44 +0800

    signup verify done

--- backend/api-server/sendmail.js
+++ backend/api-server/sendmail.js
@@ -1,25 +1,62 @@
 import nodemailer from "nodemailer";
+import { AppendingUser } from "../models/user.js";
+import { uuid } from "uuidv4";
 
-let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "hackhaha0808@gmail.com",
-    pass: "ck1060941NTU11",
-  },
-});
+const sendEmail = async (email, purpose, data) => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "hackhaha0808@gmail.com",
+      pass: "ck1060941NTU11",
+    },
+  });
 
-const mailOptions = {
-  from: "hackhaha0808@gmail.com",
-  to: "b09901031@g.ntu.edu.tw",
-  subject: "test",
-  text: "Create new account",
-  html: '<p>Click <a href="http://localhost:3000/login">here</a> to create your new account</p>',
+  let mailOptions;
+  if (purpose == "signUp") {
+    const secretToken = uuid();
+    console.log(secretToken);
+
+    const appendingData = {
+      userId: data.userId,
+      password: data.password,
+      email: data.email,
+      status: "offline",
+      gameId: "123",
+      secretToken: secretToken,
+      active: false,
+    };
+
+    const user = new AppendingUser(appendingData);
+    user.save();
+
+    mailOptions = {
+      from: "hackhaha0808@gmail.com",
+      to: email,
+      subject: "signUp",
+      text: "Create new account",
+      html: `<p>
+          Click <a href=http://localhost:4000/api/verify/${secretToken}>here</a>
+          to create your new account
+        </p>`,
+    };
+  }
+  if (purpose == "forgotPassword") {
+    mailOptions = {
+      from: "hackhaha0808@gmail.com",
+      to: email,
+      subject: "forgotPassword",
+      text: "Reset password",
+      html: '<p>Click <a href="http://localhost:3000/login">here</a> to reset your password</p>',
+    };
+  }
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 };
 
-transporter.sendMail(mailOptions, function (error, info) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Email sent: " + info.response);
-  }
-});
+export default sendEmail;
