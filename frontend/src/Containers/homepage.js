b4ac45951c69b329c6c0f5abebd6b5574a8f66e9
hackhaha1 <hankchui88@gmail.com>
2022-01-13 15:39:33 +0800

    homepage frontend done

--- frontend/src/Containers/homepage.js
+++ frontend/src/Containers/homepage.js
@@ -10,13 +10,18 @@ import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import { Login, Logout } from "../features/session/sessionSlices";
 import instance from "../instance";
 import ResponsiveAppBar from "./appbar";
-import { Snackbar, Alert } from "@mui/material";
+import { Snackbar, Alert, Stack } from "@mui/material";
+
 const theme = createTheme({
   palette: {
     quickStart: {
@@ -30,8 +35,9 @@ const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   textAlign: "center",
   color: theme.palette.text.secondary,
-  lineHeight: "60px",
+  lineHeight: "15vh",
   padding: "5vh",
+  marginBottom: "3vh",
 }));
 
 function HomePage(props) {
@@ -53,31 +59,156 @@ function HomePage(props) {
   return (
     <div>
       <ResponsiveAppBar navigate={props.navigate} />
-      <Item elevation={3}>
-        <ThemeProvider theme={theme}>
-          <Button
-            variant="contained"
-            color="quickStart"
-            style={{ width: "20vw", height: "15vh", fontSize: "2.5vw" }}
-            onClick={async () => {
-              if (!login) {
-                props.navigate("./login");
-              }
-              try {
-                await instance.post("/createRoom", {
-                  userId: userId,
-                  difficulty: "normal",
-                });
-              } catch (e) {
-                setOpen(true);
-              }
-            }}
-          >
-            QuickStart
-          </Button>
-        </ThemeProvider>
-      </Item>
-      <Grid container spacing={1}></Grid>
+
+      <Grid
+        container
+        spacing={2}
+        paddingLeft={"2vw"}
+        paddingRight={"2vw"}
+        mt={"3vh"}
+      >
+        <Grid item xs={5}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography
+                sx={{
+                  fontSize: "2.8vw",
+                  mt: "1vh",
+                  mb: "2.5vh",
+                  textAlign: "center",
+                }}
+                color="text.secondary"
+                gutterBottom
+              >
+                規則摘要
+              </Typography>
+              <Typography
+                sx={{ fontSize: "1.5vw", mb: "2.5vh" }}
+                color="text.secondary"
+              >
+                目標：研發四種解藥，讓世界不因為瘟疫而毀滅！
+              </Typography>
+              <Typography sx={{ fontSize: "1vw" }}>
+                在遊戲中，玩家化身為各個角色，利用自己有限的行動製作出4種不同瘟疫的解藥。
+              </Typography>
+              <Typography variant="body2" sx={{ fontSize: "1.2vw" }}>
+                <br />
+                每人每回合有四個行動可選：
+                <br />
+                <Grid container spacing={0.5}>
+                  <Grid item xs={6}>
+                    <Typography variant="body2" sx={{ fontSize: "1vw" }}>
+                      1. 開車/乘船
+                      <br />
+                      3. 包機航班
+                      <br />
+                      5. 設立研究所
+                      <br />
+                      7. 交換情報
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="body2" sx={{ fontSize: "1vw" }}>
+                      2. 搭乘直航
+                      <br />
+                      4. 穿梭航班
+                      <br />
+                      6. 治療病患
+                      <br />
+                      8. 研發解藥
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <br />
+                遊戲中的角色包含：
+                <br />
+                <Grid container spacing={0.5}>
+                  <Grid item xs={6}>
+                    <Typography variant="body2" sx={{ fontSize: "1vw" }}>
+                      1. 調度員
+                      <br />
+                      3. 研究員
+                      <br />
+                      5. 防疫專家
+                      <br />
+                      7. 緊急應變專員
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="body2" sx={{ fontSize: "1vw" }}>
+                      2. 科學家
+                      <br />
+                      4. 醫護人員
+                      <br />
+                      6. 營運專家
+                      <br />
+                      8. 將軍
+                    </Typography>
+                  </Grid>
+                </Grid>
+                {/* {'"a benevolent smile"'} */}
+              </Typography>
+              <br />
+              <Typography
+                sx={{ fontSize: "1.2vw", mb: "0.2vh", mt: "1.2vh" }}
+                gutterBottom
+              >
+                詳細遊戲規則：
+              </Typography>
+              <Link href="https://boardgamearena.com/link?url=https%3A%2F%2Fgamesoncloud.blogspot.com%2F2014%2F08%2Fpandemic.html&id=12657">
+                中文規則
+              </Link>
+              <br />
+              <Link href="https://boardgamearena.com/link?url=https%3A%2F%2Fwww.zmangames.com%2Fen%2Fproducts%2Fpandemic%2F&id=10284">
+                English Rulebook
+              </Link>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={7}>
+          <Stack spacing={2}>
+            <Card>
+              <CardMedia
+                component="img"
+                sx={{ maxWidth: "55vw", maxHeight: "55vh" }}
+                image="https://i.imgur.com/Q2UZka4.jpg"
+                alt="pandemic board game"
+              />
+            </Card>
+            {/* <div>
+              <img
+                src="https://i.imgur.com/Q2UZka4.jpg"
+                max-width={"10%"}
+                height={"auto"}
+              ></img>
+            </div> */}
+            <Item elevation={3}>
+              <ThemeProvider theme={theme}>
+                <Button
+                  variant="contained"
+                  // color="quickStart"
+                  style={{ width: "20vw", height: "15vh", fontSize: "2.5vw" }}
+                  onClick={async () => {
+                    if (!login) {
+                      props.navigate("./login");
+                    }
+                    try {
+                      await instance.post("/createRoom", {
+                        userId: userId,
+                        difficulty: "normal",
+                      });
+                    } catch (e) {
+                      setOpen(true);
+                    }
+                  }}
+                >
+                  QuickStart
+                </Button>
+              </ThemeProvider>
+            </Item>
+          </Stack>
+        </Grid>
+      </Grid>
 
       {/* <button
         onClick={async () => {
